7206cf80edc08e8b4d8c35adc26181ba467fdcd0
marcmillie <mrmarcusmiller0@gmail.com>
2024-10-23 20:50:20 -0400

    second commit

---
+++ fake-dating-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Signup from './components/Signup';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Fake Dating App</h1>
+      <Signup />
+    </div>
+  );
+};
+
+export default App;
---
+++ fake-dating-app/src/components/Signup.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+
+const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Signup Info', { username, email, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required
+      />
+        <input
+        type="email"
+        placeholder="Email"
+        value={username}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+        <input
+        type="password"
+        placeholder="Password"
+        value={username}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+    </form>
+  )
+}
+
+export default Signup;
